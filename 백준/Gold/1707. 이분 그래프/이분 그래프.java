151bdfe6e558ceaf617c1be727b27e6882cbf556
Sungyeon <csy357@gmail.com>
2025-03-02 16:47:00 +0900

    [Gold IV] Title: 이분 그래프, Time: 812 ms, Memory: 271804 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.java
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -10,10 +9,14 @@ import java.util.StringTokenizer;
 
 public class Main {
   static List<List<Integer>> graph;
+  static final int RED = 1;
+  static final int BLUE = -1;
+  static int[] colors;
+  static boolean checkBipartite;
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int t = Integer.parseInt(br.readLine());
-    for(int test=0; test<t; test++) {
+    while(t-->0) {
       StringTokenizer st = new StringTokenizer(br.readLine());
       int v = Integer.parseInt(st.nextToken());
       int e = Integer.parseInt(st.nextToken());
@@ -21,41 +24,60 @@ public class Main {
       graph = new ArrayList<>();
       for (int i = 0; i <= v; i++)
         graph.add(new ArrayList<>());
-      for (int i = 0; i < e; i++) {
+      while (e-->0) {
         st = new StringTokenizer(br.readLine());
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
         graph.get(a).add(b);
         graph.get(b).add(a);
       }
-      boolean[] visited = new boolean[v + 1];
-      boolean[] check = new boolean[v + 1];
-      for(int i=1; i<=v; i++)
-        if(!visited[i]) BFS(i, visited, check);
-      System.out.println(BinaryGraph(v, check));
+      colors = new int[v+1];
+      checkBipartite = true;
+      for(int i=1; i<=v; i++) {
+        if(!checkBipartite) break;
+        if(colors[i]==0) {
+          BFS(i, RED);
+        }
+      }
+      if (checkBipartite) {
+        System.out.println("YES");
+      } else {
+        System.out.println("NO");
+      }
     }
   }
-  static void BFS(int node, boolean[] visited, boolean[] check) {
+  static void BFS(int node, int color) {
     Queue<Integer> q = new LinkedList<>();
     q.add(node);
-    visited[node] = true;
-    check[node] = true;
-    while(!q.isEmpty()) {
+    colors[node] = color;
+    while(!q.isEmpty() && checkBipartite) {
       int now = q.poll();
       for(int next: graph.get(now)) {
-        check[next] = !check[now];
-        if(visited[next]) continue;
-        visited[next] = true;
-        q.add(next);
+        // 방문하지 않은 정점이라면
+        if(colors[next]==0) {
+          q.add(next);
+          colors[next] = colors[now] *-1;
+        }
+        // 서로 인접한 정점의 색이 같은 색이면 이분 그래프가 아니다.
+        else if (colors[now]+colors[next] != 0) {
+          checkBipartite = false;
+          return;
+        }
       }
     }
   }
-  static String BinaryGraph(int v, boolean[] check) {
-    for(int i=1; i<=v; i++) {
-      for(int next: graph.get(i)) {
-        if(check[next]==check[i]) return "NO";
+  static void DFS(int node, int color) {
+    colors[node] = color;
+    for(int next: graph.get(node)) {
+      // 시작 정점의 색과 인접한 정점의 색이 같으면 이분 그래프가 아니다.
+      if(colors[next] == color) {
+        checkBipartite = false;
+        return;
+      }
+      // 시작 정점과 인접한 정점이 방문하지 않은 정점이면 dfs 실행
+      if(colors[next]==0) {
+        DFS(next, colors[node]*-1);
       }
     }
-    return "YES";
   }
 }
