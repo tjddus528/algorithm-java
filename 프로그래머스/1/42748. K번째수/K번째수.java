e21ae97df5837f9ac49716a1aac258c3b9057846
Sungyeon <csy357@gmail.com>
2025-03-06 15:06:19 +0900

    [level 1] Title: K번째수, Time: 0.52 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        for(int i=0; i<commands.length;i++) {
+            int[] ans = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            Arrays.sort(ans);
+            answer[i] = ans[commands[i][2]-1];
+        }
+        return answer;
+    }
+}
