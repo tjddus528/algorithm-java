ac1e73762c93594e6c71f28af913be227d52d707
Sungyeon <csy357@gmail.com>
2025-03-06 15:03:41 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 4.21 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -3,11 +3,6 @@ class Solution {
     public int solution(int[][] jobs) {
         // 우선순위큐 -> 소요 시간 기준 오름차순 정렬
         PriorityQueue<int[]> pq = new PriorityQueue<>((o1,o2)-> o1[1]-o2[1]);
-        // {
-        //     if(o1[1]==o2[1]) return o1[0]-o2[0];
-        //     else if (o1[1] > o2[1]) return o1[1]-o2[1];
-        //     else return -1;
-        //     });
         // 기존 배열 -> 요청 시간 기준 오름차순 정렬
         Arrays.sort(jobs, (o1, o2)->o1[0]-o2[0]);
         
@@ -17,25 +12,18 @@ class Solution {
         int process = 0;
         
         while(true) {
-            // System.out.println("time:" +time);
             // 현재 time 이전까지 요청된 작업들 pq에 추가
             while(k<jobs.length && jobs[k][0] <= time) {
                 pq.add(jobs[k]);
                 k++;
             }
-            // PriorityQueue<int[]> temp = new PriorityQueue<>(pq);
-            // while(!temp.isEmpty()) {
-            //     System.out.print(Arrays.toString(temp.poll()) + " ");
-            // }
-            // System.out.println();
-            // time내에 요청된 작업이 없지만, 
-            // 아직 작업들이 남아있다면 time을 늘려서 계속 진행
-            // 남은 작업이 없으면 break;
+            // time내에 요청된 작업이 없을 때
             if (pq.isEmpty()) {
-                if(k<jobs.length) time++;
-                else break;
+                if(k<jobs.length) time++; // 아직 작업들이 남아있다면 time을 늘려서 계속 진행
+                else break;               // 남은 작업이 없으면 break;
             }
-            // 현재 시점에서 처리할 작업들이 우선순위에 따라 순차적으로 나옴
+            // 현재 시점에서 처리할 작업들 중 가장 우선순위가 높은 작업 처리 
+            // (time이 갱신되므로 그 사이 새로운 작업이 요청될 수 있으므로 1개만 처리하고 넘어감)
             else {
                 int[] job = pq.poll();
                 time += job[1];
