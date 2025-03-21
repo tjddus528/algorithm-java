de1f4e3555c66f1a738a4f4884ec5884165e2921
Sungyeon <csy357@gmail.com>
2025-03-21 20:39:16 +0900

    [level 3] Title: 이중우선순위큐, Time: 109.71 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -3,26 +3,20 @@ class Solution {
     public int[] solution(String[] operations) {
         int[] answer = {};
         PriorityQueue<Integer> min = new PriorityQueue<>();
-        PriorityQueue<Integer> temp = new PriorityQueue<>();
+        PriorityQueue<Integer> max = new PriorityQueue<>(Collections.reverseOrder());
         for(String oper: operations) {
             String[] data = oper.split(" ");
             if(data[0].equals("I")) {
                 min.offer(Integer.parseInt(data[1]));
+                max.offer(Integer.parseInt(data[1]));
             }
             else if (data[1].equals("1")) {         // 최댓값 삭제
-                while(min.size()>1) {
-                    temp.offer(min.poll());
-                }
-                min = temp;
-                temp = new PriorityQueue<>();
+                min.remove(max.poll());
             } else {                                // 최솟값 삭제
-                min.poll();
+                max.remove(min.poll());
             }
         }
-        if(min.size()==0) return new int[]{0,0};
-        int first = min.poll();
-        int last = first;
-        while(!min.isEmpty()) last = min.poll();
-        return new int[]{last, first};
+        if(min.isEmpty() && max.isEmpty()) return new int[]{0,0};
+        else return new int[]{max.poll(), min.poll()};
     }
 }
