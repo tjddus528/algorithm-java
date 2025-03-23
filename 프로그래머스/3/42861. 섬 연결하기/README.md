37331147da722908d557cc896d945979976bd31b
Sungyeon <csy357@gmail.com>
2025-03-23 22:43:58 +0900

    [level 3] Title: 섬 연결하기, Time: 0.66 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/README.md
+++ 프로그래머스/3/42861. 섬 연결하기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 섬 연결하기 - 42861 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861) 
 
 ### 성능 요약
 
-메모리: 75.7 MB, 시간: 0.87 ms
+메모리: 75.4 MB, 시간: 0.66 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 23일 22:40:53
+2025년 03월 23일 22:43:56
 
 ### 문제 설명
 
--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -3,23 +3,30 @@ class Solution {
     static int[] node;
     public int solution(int n, int[][] costs) {
         int answer = 0;
+        // cost기준으로 정렬
+        Arrays.sort(costs, (o1, o2) -> o1[2]-o2[2]);
+        
+        // 집합의 부모 노드 배열 초기화
         node = new int[n+1];
         for(int i=1; i<=n; i++) node[i] = i;
-        Arrays.sort(costs, (o1, o2) -> o1[2]-o2[2]);
+        
+        // 모든 노드가 같은 부모를 가질 때까지 (즉, 모두 합집합시키기)
         for(int[] cost: costs) {
-            if(find(cost[0]) != find(cost[1])) {
+            if(find(cost[0]) != find(cost[1])) {    // 다른 집합이라면 union 연산, cost추가
                 union(cost[0], cost[1]);
                 answer+= cost[2];
             }
         }
         return answer;
     }
+    // 두 노드가 속한 집합의 부모 노드를 통일시켜주기 -> 합집합
     static void union(int a, int b) {
         int pa = find(a);
         int pb = find(b);
         if(pa<pb) node[pa] = pb;
         else node[pb] = pa;
     }
+    // 부모 노드 찾기
     static int find(int x) {
         if(node[x] == x) return x;
         return find(node[x]);
