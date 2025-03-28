344e7b88892c72b64d90b0a73820f3807986d1e7
Sungyeon <csy357@gmail.com>
2025-03-28 23:40:01 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 16.76 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -6,10 +6,14 @@ class Solution {
         int total = 0;
         int idx = 0;
         int now = 0;
+        // 모든 트럭이 다리위에 올라갈 때까지 반복문
+        // 트럭을 큐에 넣을 때에는 {idx, weight, 다리를 다 건너는 시간(현재시간+다리길이)}
         while(idx < n) {
+            // 큐 맨앞 원소가 다리를 다 건너는 시간이 되면 poll, 총합무게에서 제외
             if(!q.isEmpty() && q.peek()[2] <= now) {
                 total -= q.poll()[1];
             }
+            // 동시에 다리에 오를 수 있는 조건을 만족하면 계속 추가(무게 추가, idx++)
             while (q.size() < bridge_length && idx < n && total+truck_weights[idx] <= weight) {
                 q.add(new int[]{idx, truck_weights[idx], now+bridge_length});
                 total += truck_weights[idx];
@@ -17,7 +21,9 @@ class Solution {
             }
             now++;
         }
+        // 모든 트럭을 다리 위에 놓은 뒤에 아직 다 건너지 못한 트럭이 있다면
         while(!q.isEmpty()) {
+            // 트럭이 다 건너는 시간까지 기다렸다가 poll
             if(q.peek()[2] <= now) q.poll();
             now++;
         }
