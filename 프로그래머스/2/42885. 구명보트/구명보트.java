3274f5f7351e98dc8ea541c6b0638e39a9b9df7c
Sungyeon <csy357@gmail.com>
2025-04-04 20:45:12 +0900

    [level 2] Title: 구명보트, Time: 37.41 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -2,23 +2,24 @@ import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
+        // 양방향 큐 => 덱 : 오름차순 정렬
         ArrayDeque<Integer> deq = new ArrayDeque<>();
         Arrays.sort(people);
         for(int w: people) {
             deq.offer(w);
         }
+        // 양쪽에서 하나씩 (큰수 하나/ 큰수+작은수)
         Stack<Integer> stack = new Stack<>();
         while(!deq.isEmpty()) {
             stack.add(deq.pollLast());
+            answer++;
             if (deq.isEmpty()) {
                 stack.pop();
-                answer++;
                 break;
             }
             if (stack.pop() + deq.peekFirst() <= limit) {
                 deq.pollFirst();
             }
-            answer++;
         }
         return answer;
     }
