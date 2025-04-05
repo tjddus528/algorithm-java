8c2923aa11d2d959a8dd6a52e71cb6e9bbccf9de
Sungyeon <csy357@gmail.com>
2025-04-06 01:22:43 +0900

    [level 3] Title: 단속카메라, Time: 9.92 ms, Memory: 56.6 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/README.md
+++ 프로그래머스/3/42884. 단속카메라/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단속카메라 - 42884 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42884?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42884?language=java#) 
 
 ### 성능 요약
 
-메모리: 58.5 MB, 시간: 8.67 ms
+메모리: 56.6 MB, 시간: 9.92 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 06일 01:21:20
+2025년 04월 06일 01:22:40
 
 ### 문제 설명
 
--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class Solution {
     public int solution(int[][] routes) {
-        Arrays.sort(routes, (o1, o2)->o1[0]-o2[0]);
+        Arrays.sort(routes, (o1, o2)->o1[1]-o2[1]);
         int end = routes[0][1];
         int answer = 1;
         for(int i=1; i<routes.length; i++) {
@@ -9,10 +9,7 @@ class Solution {
             if(end < routes[i][0]) {
                 answer++;
                 end = routes[i][1];
-            } 
-            else 
-                end = Math.min(end, routes[i][1]);
-            
+            }
         }
         return answer;
     }
