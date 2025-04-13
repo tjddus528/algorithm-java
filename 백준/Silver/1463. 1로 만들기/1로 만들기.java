7b0c4251926116da7a2b2871081bbc78b489d247
Sungyeon <csy357@gmail.com>
2025-04-13 09:00:33 +0900

    [Silver III] Title: 1로 만들기, Time: 132 ms, Memory: 18144 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -2,28 +2,25 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.HashSet;
-import java.util.Set;
 
 public class Main {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int x = Integer.parseInt(br.readLine());
 
-    // BFS
-    ArrayDeque<int[]> q = new ArrayDeque<>();
-    q.offer(new int[]{x, 0});
-    int result = 0;
-    while(!q.isEmpty()) {
-      int[] num = q.poll();
-      if(num[0]==1) {
-        result = num[1];
-        break;
-      }
-      if(num[0] % 3==0) q.offer(new int[]{num[0]/3, num[1]+1});
-      if(num[0] % 2==0) q.offer(new int[]{num[0]/2, num[1]+1});
-      q.offer(new int[]{num[0]-1, num[1]+1});
+    // DP 테이블 (각 숫자를 1로 만드는 데 필요한 최소 연산 횟수)
+    int[] dp = new int[x + 1];
+    for (int i= 2; i<= x; i++) {
+      // 1을 빼는 경우
+      dp[i] = dp[i - 1] + 1;
+      // 2로 나누어 떨어지는 경우
+      if(i % 2== 0)
+        dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+      // 3으로 나누어 떨어지는 경우
+      if(i% 3 == 0)
+        dp[i] = Math.min(dp[i], dp[i / 3] + 1);
     }
-    System.out.println(result);
+    System.out.println(dp[x]);
+
   }
 }
