a256394e94eda48a0c58af57bd151ef9d0362231
Sungyeon <csy357@gmail.com>
2025-05-16 08:56:04 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 100 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -9,24 +9,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        long[][] dp = new long[N+1][10];
-        for(int i=1; i<=9; i++) dp[0][i] = 1;
-        for(int idx=0; idx<N; idx++) {
-            for(int i=0; i<10; i++) {
-                if(dp[idx][i] > 0) {
-                    if(i-1>=0) {
-                        dp[idx+1][i-1] += dp[idx][i];
-                        dp[idx+1][i-1] %= DIV;
-                    }
-                    if(i+1<10) {
-                        dp[idx+1][i+1] += dp[idx][i];
-                        dp[idx+1][i+1] %= DIV;
-                    }
-                }
+        long[][] dp = new long[N + 1][10];
+        for (int i = 1; i <= 9; i++) dp[1][i] = 1;
+        for (int idx = 2; idx <= N; idx++) {
+            dp[idx][0] = dp[idx - 1][1];
+            dp[idx][9] = dp[idx - 1][8];
+            for (int i = 1; i < 9; i++) {
+                dp[idx][i] = (dp[idx - 1][i - 1] + dp[idx-1][i + 1]) % DIV;
             }
         }
         long result = 0;
-        for(long d: dp[N-1]) result = (result+d)%DIV;
-        System.out.println(result%DIV);
+        for (long d : dp[N])
+            result = (result + d) % DIV;
+        System.out.println(result % DIV);
     }
 }
