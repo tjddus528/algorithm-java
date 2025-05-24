9aac98eae4740bac4ee2f35482ff25168e1478f9
Sungyeon <csy357@gmail.com>
2025-05-25 03:06:02 +0900

    [Silver III] Title: N과 M (8), Time: 136 ms, Memory: 17024 KB -BaekjoonHub

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.java
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -1,37 +1,35 @@
-import java.io.*;
-import java.util.*;
-public class Main {
-	static int N,M,arr[],result[];
-	public static void comb(int start, int cnt) {
-		if(M == cnt) {
-			for(int i : result) {
-				System.out.print(i+" ");
-			}
-			System.out.println();
-			return;
-		}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
-		for(int i=start; i<N; i++) {
-			result[cnt] = arr[i];
-			comb(i,cnt+1);
-		}
-	}
-	public static void main(String[] args) throws Exception {
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	    StringTokenizer st = new StringTokenizer(br.readLine());
-	    N = Integer.parseInt(st.nextToken());
-	    M = Integer.parseInt(st.nextToken());
-	    arr = new int[N];
-	    result = new int[M];
-	    st = new StringTokenizer(br.readLine());
-	    for(int i=0; i<N; i++) {
-	    	arr[i] = Integer.parseInt(st.nextToken());
-	    }
-	    Arrays.sort(arr);
-	    comb(0,0);
-	    bw.flush();
-	    bw.close();
-	    br.close();
+public class Main {
+    static int N, M;
+    static int[] arr;
+    static int[] output;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] data = br.readLine().split(" ");
+        N = Integer.parseInt(data[0]);
+        M = Integer.parseInt(data[1]);
+        arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(arr);
+        output = new int[M];
+        dupCombi(0, 0);
+        System.out.println(sb);
+    }
+    static void dupCombi(int depth, int start) {
+        if(start >= N) return;
+        if(depth == M) {
+            for (int ans : output) {
+                sb.append(ans).append(" ");
+            }
+            sb.append('\n');
+            return;
+        }
+        output[depth] = arr[start];
+        dupCombi(depth+1, start);
+        dupCombi(depth, start+1);
     }
 }
