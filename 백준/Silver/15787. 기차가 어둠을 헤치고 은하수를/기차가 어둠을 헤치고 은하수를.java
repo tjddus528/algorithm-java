3ec15543bf993f43decd6725cff817a458f8f865
Sungyeon <csy357@gmail.com>
2025-06-08 10:05:18 +0900

    [Silver II] Title: 기차가 어둠을 헤치고 은하수를, Time: 416 ms, Memory: 48116 KB -BaekjoonHub

--- 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.java
+++ 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.java
@@ -6,45 +6,45 @@ import java.util.Set;
 
 public class Main {
     static int N, M;
-    static int[][] arr;
+    static int[] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] data = br.readLine().split(" ");
         N = Integer.parseInt(data[0]);
         M = Integer.parseInt(data[1]);
-        arr = new int[N+1][21];
+
+        /**
+         * 비트마스킹
+         */
+        arr = new int[N+1];
         for(int i=0; i<M; i++) {
             String[] input = br.readLine().split(" ");
             int a = Integer.parseInt(input[0]);
             int b = Integer.parseInt(input[1]);
-            int c = 0;
-            if(a==1 || a==2) c = Integer.parseInt(input[2]);
+            int x = -1;
 
-            if(a==1) arr[b][c] = 1;
-            if(a==2) arr[b][c] = 0;
-            if(a==3) {
-                for(int j=20; j>=2; j--) {
-                    arr[b][j] = arr[b][j-1];
-                }
-                arr[b][1] = 0;
-            }
-            if(a==4) {
-                for(int j=1; j<=19; j++) {
-                    arr[b][j] = arr[b][j+1];
-                }
-                arr[b][20] = 0;
+            switch (a) {
+                case 1:
+                    x += Integer.parseInt(input[2]);
+                    arr[b] |= (1 << x);
+                    break;
+                case 2:
+                    x += Integer.parseInt(input[2]);
+                    arr[b] &= ~(1 << x);
+                    break;
+                case 3:
+                    arr[b] <<= 1;
+                    // 20비트만 유지 (초과 비트 제거)
+                    arr[b] &= (1 << 20) - 1;
+                    break;
+                case 4:
+                    arr[b] >>= 1;
+                    break;
             }
         }
-        Set<String> train = new HashSet<>();
-        for(int i=1; i<=N; i++) {
-            String code = "";
-            for(int j=1; j<=20; j++) {
-                code += String.valueOf(arr[i][j]);
-            }
-            train.add(code);
-        }
-        System.out.println(train.size());
-
+        Set<Integer> set = new HashSet<>();
+        for(int i=1; i <= N; i++) set.add(arr[i]);
+        System.out.println(set.size());
     }
 
 }
