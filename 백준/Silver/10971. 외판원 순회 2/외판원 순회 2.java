94fcc11d5082a9042ba809c56c9a22666ca0cdd5
Sungyeon <csy357@gmail.com>
2025-06-15 10:43:37 +0900

    [Silver II] Title: 외판원 순회 2, Time: 296 ms, Memory: 14556 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -1,27 +1,35 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
-
 public class Main {
     static int N;
     static int[][] arr;
     static int result = Integer.MAX_VALUE;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         N = Integer.parseInt(br.readLine());
         arr = new int[N][N];
-        for(int i=0; i<N; i++) {
+
+        for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int j=0; j<N; j++) {
+            for (int j = 0; j < N; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        for(int i=0; i<N; i++){
+
+        for (int i = 0; i < N; i++) {
             bt(i, 0, i, new boolean[N], 0);
         }
-        System.out.println(result);
 
+        bw.write(String.valueOf(result));
+        bw.flush();
+        bw.close();
+        br.close(); // 이 줄도 추가하면 좋습니다.
     }
     static void bt(int start, int depth, int index, boolean[] visited, int sum) {
         if(depth == N-1) {
