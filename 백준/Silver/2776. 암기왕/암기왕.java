417783cbe90ab503a1d888a53da0f52853412967
Sungyeon <csy357@gmail.com>
2025-06-22 01:26:13 +0900

    [Silver IV] Title: 암기왕, Time: 1444 ms, Memory: 285092 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.java
+++ 백준/Silver/2776. 암기왕/암기왕.java
@@ -1,73 +1,31 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
- 
- 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
 public class Main {
-	
-	public static int T, N, M;
-	public static int[] arr;
-	public static int answer = 0;
-	public static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) throws IOException{
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	
-    	StringTokenizer st = new StringTokenizer(br.readLine());
-    	T = Integer.parseInt(st.nextToken());
-    	
-    	for(int t=0;t<T;t++) {
-            sb = new StringBuilder();
-    		
-	    	st = new StringTokenizer(br.readLine());
-	    	N = Integer.parseInt(st.nextToken());
-	    	
-	    	arr = new int[N];
-	    	st = new StringTokenizer(br.readLine());
-	    	for(int i=0;i<N;i++) {
-	    		arr[i] =  Integer.parseInt(st.nextToken());
-	    	}
-	    	
-	    	Arrays.sort(arr);
-	    	
-	    	
-    		st = new StringTokenizer(br.readLine());
-        	M = Integer.parseInt(st.nextToken());
-        	
-        	st = new StringTokenizer(br.readLine());
-        	for(int j=0;j<M;j++) {
-        		binarySearch(Integer.parseInt(st.nextToken()));
-        	}
-	        	
-        	System.out.print(sb);
-    	}
- 
-    	
-    }
-    
-    public static void binarySearch(int target) {
-    	int start = 0;
-    	int end = N - 1;
-    	
-    	while(start <= end) {
-    		int middle = (start + end) / 2;
-    		
-    		if(arr[middle] == target) {
-    			sb.append('1').append("\n");
-//    			System.out.println("1");
-    			return ;
-    		}
-    		
-    		if(arr[middle] < target) {
-    			start = middle + 1;
-    		}else {  // while(start <= end) 이기에 end를 더 작게만들어줘야함
-    			end = middle - 1;
-    		}
-    		
-    	}
-    	sb.append('0').append("\n");
-//		System.out.println("0");
-    	
-    	
+    static int test;
+    static int N, M;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        test = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        while(test --> 0) {
+            N = Integer.parseInt(br.readLine());
+            Set<Integer> note = new HashSet<>();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            while(N --> 0) {
+                note.add(Integer.parseInt(st.nextToken()));
+            }
+            M = Integer.parseInt(br.readLine());
+            st = new StringTokenizer(br.readLine());
+            while(M --> 0) {
+                int num = Integer.parseInt(st.nextToken());
+                sb.append(note.contains(num) ? 1+"\n": 0+"\n");
+            }
+        }
+        System.out.println(sb);
     }
 }
