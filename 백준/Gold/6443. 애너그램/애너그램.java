ebef0688568edaf628f484c184834deb15fdbcf9
Sungyeon <csy357@gmail.com>
2025-06-24 01:22:18 +0900

    [Gold V] Title: 애너그램, Time: 492 ms, Memory: 73736 KB -BaekjoonHub

--- 백준/Gold/6443. 애너그램/애너그램.java
+++ 백준/Gold/6443. 애너그램/애너그램.java
@@ -1,49 +1,43 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Main {
+    static int N;
+    static char[] anagram;
+    static int[] check;
+    static Set<String> result;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        while(N --> 0) {
+            anagram = br.readLine().toCharArray();
+            check = new int[26];
+            for(char c: anagram) {
+                check[c - 'a']++;
+            }
+            result = new TreeSet<>();
+            perm(0, "");
+            for(String s: result) sb.append(s).append("\n");
+        }
+        System.out.println(sb);
+        
+    }
+    static void perm(int depth, String str) {
+        if(depth == anagram.length) {
+            result.add(str);
+            return;
+        }
 
-	static char[] arr;
-	static int[] check;
-	static StringBuilder sb;
-	static Stack<Character> s;
-	static Set<String> set;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringBuilder result = new StringBuilder();
-		int n = Integer.parseInt(br.readLine());
-		while(n-- > 0) {
-			set = new TreeSet<>();
-			arr = br.readLine().toCharArray();
-			check = new int[26];
-			for(char c : arr) {
-				check[c-'a']++;
-			}
-			s = new Stack<>();
-			comb(arr.length);
-			set.stream().forEach(s -> result.append(s).append("\n"));
-		}
-		System.out.println(result.toString());
-	}
-	
-	static void comb(int r) {
-		if(r == s.size()) {
-			StringBuilder sb = new StringBuilder();
-			for(char c : s) {
-				sb.append(c);
-			}
-			set.add(sb.toString());
-		}
-		
-		for(int i=0; i<26; i++){
-			if(check[i] > 0) {
-				check[i]--;
-				s.push((char)(i+'a'));
-				comb(r);
-				s.pop();
-				check[i]++;
-			}
-		}
-	}
+        for (int i=0; i<26; i++) {
+            if (check[i] == 0)
+                continue;
+            check[i]--;
+            perm(depth + 1, str + (char)(i+'a'));
+            check[i]++;
+        }
+    }
 }
