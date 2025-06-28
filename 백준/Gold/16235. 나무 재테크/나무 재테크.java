46f23abedfab28e5f47d0280650f2f53e1ec4a4a
Sungyeon <csy357@gmail.com>
2025-06-29 03:00:38 +0900

    [Gold III] Title: 나무 재테크, Time: 752 ms, Memory: 295688 KB -BaekjoonHub

--- 백준/Gold/16235. 나무 재테크/나무 재테크.java
+++ 백준/Gold/16235. 나무 재테크/나무 재테크.java
@@ -1,114 +1,109 @@
-import java.io.*;
-import java.util.*;
-
-// N: 맵의 크기
-// M: 나무의 개수
-// K: K년이 지난 후
-// A[r][c]: 추가되는 양분의 양
-// x y z: 나무의 위치, 나이
-
-class Tree implements Comparable<Tree> {
-	int x, y, age;
-
-	public Tree(int x, int y, int age) {
-		super();
-		this.x = x;
-		this.y = y;
-		this.age = age;
-	}
-
-	@Override
-	public int compareTo(Tree o) {
-		return this.age - o.age;
-	}
-}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class Main {
-	static int[] adj_x = { -1, -1, -1, 0, 0, 1, 1, 1 };
-	static int[] adj_y = { -1, 0, 1, -1, 1, -1, 0, 1 };
+    static int N, M, K;
+    static int[][] ground;
+    static int[][] A;
+    static List<int[]> tree;
+    static List<int[]> death;
+    static boolean[] isDeath;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] data = br.readLine().split(" ");
+        N = Integer.parseInt(data[0]);
+        M = Integer.parseInt(data[1]);
+        K = Integer.parseInt(data[2]);
+        ground = new int[N+1][N+1];
+        for(int i=1; i<=N; i++) Arrays.fill(ground[i], 5);
+//        System.out.println(Arrays.deepToString(ground));
+        A = new int[N+1][N+1];
+        tree = new ArrayList<>();
+        death = new ArrayList<>();
+        isDeath = new boolean[M];
+        for(int i=1; i<=N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j=1; j<=N; j++) {
+                A[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for(int i=0; i<M; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int age = Integer.parseInt(st.nextToken());
+            tree.add(new int[]{x, y, age, 1});
+        }
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+        // tree : 나이 어린 순으로 정렬
+        while(K --> 0) {
+            tree.sort(Comparator.comparing(a -> a[2]));
+            spring();
+            summer();
+            fall();
+            winter();
+        }
 
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		int[][] A = new int[N + 1][N + 1];
-		int[][] eat = new int[N + 1][N + 1]; // 양분
-		Deque<Tree> tree_list = new LinkedList<>();
+        System.out.println(tree.size());
 
-		// A[r][c] 입력
-		for (int i = 1; i <= N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 1; j <= N; j++) {
-				A[i][j] = Integer.parseInt(st.nextToken());
-				eat[i][j] = 5;
-			}
-		}
 
-		// 나무 리스트에 추가
-		for (int i = 1; i <= M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int x = Integer.parseInt(st.nextToken());
-			int y = Integer.parseInt(st.nextToken());
-			int tree_age = Integer.parseInt(st.nextToken());
-			tree_list.add(new Tree(x, y, tree_age));
-		}
 
-		while (K > 0) {
-			Queue<Tree> die_tree_list = new LinkedList<>();
+    }
+    static void spring() {
+        List<int[]> survivors = new ArrayList<>();
+        death.clear();  // 지난 해 죽은 거 남아있지 않게
 
-			// 봄
-			for (int i = 0; i < tree_list.size();) {
-				Tree cur = tree_list.poll();
-				if (eat[cur.x][cur.y] >= cur.age) {
-					eat[cur.x][cur.y] -= cur.age;
-					cur.age++;
-					i++;
-					tree_list.add(cur);
-				} else {
-					die_tree_list.add(cur);
-				}
-			}
-
-			// 여름
-			for (Tree t : die_tree_list) {
-				eat[t.x][t.y] += t.age / 2;
-			}
-
-			// 가을
-			Queue<Tree> temp_list = new LinkedList<>();
-			for (Tree t : tree_list) {
-				if (t.age % 5 == 0) {
-					temp_list.add(t);
-				}
-			}
-			while (!temp_list.isEmpty()) {
-				Tree t = temp_list.poll();
-
-				for (int i = 0; i < 8; i++) {
-					int next_x = t.x + adj_x[i];
-					int next_y = t.y + adj_y[i];
-					if (next_x >= 1 && next_x <= N && next_y >= 1 && next_y <= N) {
-						tree_list.addFirst(new Tree(next_x, next_y, 1));
-					}
-				}
-			}
-
-			// 겨울
-			for (int i = 1; i <= N; i++) {
-				for (int j = 1; j <= N; j++) {
-					eat[i][j] += A[i][j];
-				}
-			}
-
-			K--;
-		}
-
-		bw.write(tree_list.size() + "\n");
-		bw.flush();
-		bw.close();
-	}
+        for (int[] t : tree) {
+            int x = t[0], y = t[1], age = t[2];
+            if (ground[x][y] >= age) {
+                ground[x][y] -= age;
+                t[2] = age + 1;
+                survivors.add(t);
+            } else {
+                death.add(t);
+            }
+        }
+        tree = survivors;  // 죽은 나무 제거
+    }
+    static void summer() {
+        for(int[] t: death) {
+            int x = t[0];
+            int y = t[1];
+            int age = t[2];
+            ground[x][y] += (age/2);
+        }
+        death.clear();
+    }
+    static void fall() {
+        int[] dx = new int[]{-1,-1,-1, 0, 0, 1, 1, 1};
+        int[] dy = new int[]{-1, 0, 1,-1, 1,-1, 0, 1};
+        List<int[]> newTrees = new ArrayList<>();
+        for (int[] t: tree) {
+            if (t[2] % 5 == 0) {
+                for (int d = 0; d < 8; d++) {
+                    int nx = t[0] + dx[d];
+                    int ny = t[1] + dy[d];
+                    if (nx < 1 || ny < 1 || nx > N || ny > N) continue;
+                    newTrees.add(new int[]{nx, ny, 1});
+                }
+            }
+        }
+        tree.addAll(newTrees);
+    }
+    static void winter() {
+        for(int i=1; i<=N; i++) {
+            for(int j=1; j<=N; j++) {
+                ground[i][j] += A[i][j];
+            }
+        }
+    }
 }
+
