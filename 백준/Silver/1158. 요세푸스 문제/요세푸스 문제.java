791f22ed37cf51f1a0f5ab64218f9c4beb8b07fb
Sungyeon <csy357@gmail.com>
2025-07-07 00:10:50 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 124 ms, Memory: 14736 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,47 +1,37 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
- 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
- 
-    public static void main(String[] args) throws Exception {
+    static int N, K;
+    static List<Integer> people = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
- 
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
- 
-        Queue<Integer> q = new LinkedList<>();
-        
-        // Queue에 1~N까지 값을 offer한다.
-        for (int i = 1; i <= N; i++) {
-            q.offer(i);
+        String[] data = br.readLine().split(" ");
+        N = Integer.parseInt(data[0]);
+        K = Integer.parseInt(data[1]);
+        for(int i=1; i<=N; i++)
+            people.add(i);
+        List<Integer> order = new ArrayList<>();
+        int index = K-1;
+        while (!people.isEmpty()) {
+            int get  = people.get(index);
+            order.add(get);
+
+            people.remove(index);
+            if(people.isEmpty()) break;
+
+            index = (index + K-1) % people.size();
         }
- 
         StringBuilder sb = new StringBuilder();
         sb.append("<");
-        
-        // Queue의 사이즈가 1일 때까지 반복한다.
-        while(q.size() != 1) {
-            // K - 1번째까지는 처음에 있던 값을 맨 뒤로 보낸다.
-            for (int i = 0; i < K - 1; i++) {
-                q.offer(q.poll());
-            }
-            // K번째 값은 poll한 후 출력한다.
-            sb.append(q.poll() + ", ");
+        for(int i=0; i<order.size(); i++) {
+            sb.append(order.get(i));
+            if(i==order.size()-1) sb.append(">");
+            else sb.append(", ");
         }
-        
-        // Queue의 사이즈가 1일 때는 단순히 poll하면 된다.
-        sb.append(q.poll() + ">");
- 
-        bw.write(sb.toString() + "\n");
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(sb);
     }
 }
