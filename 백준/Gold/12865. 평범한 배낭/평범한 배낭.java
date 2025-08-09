87186f59736bd8d085efbd6db2df9d87fe200633
Sungyeon <csy357@gmail.com>
2025-08-10 08:50:12 +0900

    [Gold V] Title: 평범한 배낭, Time: 172 ms, Memory: 53764 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -5,31 +5,33 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-  static int n;
-  static int[][] dp;
-  static boolean[] visited;
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    String[] data = br.readLine().split(" ");
-    n = Integer.parseInt(data[0]);
-    int k = Integer.parseInt(data[1]);
-    int[][] bags = new int[n+1][2];
-    for(int i=1; i<=n; i++) {
-      StringTokenizer st = new StringTokenizer(br.readLine());
-      bags[i][0] = Integer.parseInt(st.nextToken());
-      bags[i][1] = Integer.parseInt(st.nextToken());
-    }
-    dp = new int[n+1][k+1];
-    visited = new boolean[k+1];
-    for(int w = 1; w <=k; w++) {
-      for (int i = 1; i <= n; i++) {
-        if (bags[i][0] > w) {
-          dp[i][w] = dp[i - 1][w];
-        } else {
-          dp[i][w] = Math.max(dp[i - 1][w], bags[i][1] + dp[i - 1][w - bags[i][0]]);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[N+1][2];
+        for(int i=1; i<=N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
         }
-      }
+
+        // 정렬은 필수는 아님
+//        Arrays.sort(arr, (o1, o2) -> o1[0]-o2[0]);
+        int[][] dp = new int[N+1][K+1];
+        for(int i=1; i<=N; i++) {
+            for(int j=1; j<=K; j++) {
+                // 현재 물건의 무게가 j무게보다 커서 배낭에 넣을 수 없을 때
+                if(arr[i][0] > j) dp[i][j] = dp[i-1][j];
+                // 현재 물건의 무게가 j무게보다 작아서 배낭에 넣을 수 있을 때
+                else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-arr[i][0]]+arr[i][1]);
+                }
+            }
+        }
+        System.out.println(dp[N][K]);
+
+
     }
-    System.out.println(dp[n][k]);
-  }
 }
