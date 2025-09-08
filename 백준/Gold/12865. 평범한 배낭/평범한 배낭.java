b4c76bc0979874cbbb78782b978c0b88dcf18cf9
Sungyeon <csy357@gmail.com>
2025-09-08 11:45:43 +0900

    [Gold V] Title: 평범한 배낭, Time: 144 ms, Memory: 16112 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,37 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
+// 평범한 배낭
 public class Main {
+    static int N, K;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-        int[][] arr = new int[N+1][2];
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        int[] D = new int[K+1];
+
         for(int i=1; i<=N; i++) {
             st = new StringTokenizer(br.readLine());
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = Integer.parseInt(st.nextToken());
-        }
+            int V = Integer.parseInt(st.nextToken());
+            int C = Integer.parseInt(st.nextToken());
 
-        // 정렬은 필수는 아님
-//        Arrays.sort(arr, (o1, o2) -> o1[0]-o2[0]);
-        int[][] dp = new int[N+1][K+1];
-        for(int i=1; i<=N; i++) {
-            for(int j=1; j<=K; j++) {
-                // 현재 물건의 무게가 j무게보다 커서 배낭에 넣을 수 없을 때
-                if(arr[i][0] > j) dp[i][j] = dp[i-1][j];
-                // 현재 물건의 무게가 j무게보다 작아서 배낭에 넣을 수 있을 때
-                else {
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-arr[i][0]]+arr[i][1]);
-                }
+            for(int w=K; w>=1; w--) {
+                if(w < V) continue;
+                D[w] = Math.max(D[w], D[w-V]+C);
             }
         }
-        System.out.println(dp[N][K]);
-
-
+        System.out.println(D[K]);
     }
+
 }
