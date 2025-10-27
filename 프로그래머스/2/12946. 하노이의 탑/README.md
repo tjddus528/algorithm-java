de0b146e849781421a86fbe5a56381daf29bf41d
Sungyeon <csy357@gmail.com>
2025-10-27 17:00:21 +0900

    [level 2] Title: 하노이의 탑, Time: 7.59 ms, Memory: 98.2 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/README.md
+++ 프로그래머스/2/12946. 하노이의 탑/README.md
@@ -1,10 +1,10 @@
 # [level 2] 하노이의 탑 - 12946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12946?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12946) 
 
 ### 성능 요약
 
-메모리: 111 MB, 시간: 8.00 ms
+메모리: 98.2 MB, 시간: 7.59 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 27일 16:56:05
+2025년 10월 27일 17:00:19
 
 ### 문제 설명
 
--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -2,38 +2,23 @@ import java.util.*;
 class Solution {
     static List<int[]> order = new ArrayList<>();
     public int[][] solution(int n) {
-        solve(n, 1, 3);
-        // for(int[] od: order) {
-        //     System.out.print("["+od[0] + ", " + od[1]+"] ");
-        // }
+        solve(n, 1, 3, 2);
         int[][] answer = new int[order.size()][2];
         for(int i=0; i<order.size(); i++) {
             answer[i][0] = order.get(i)[0];
             answer[i][1] = order.get(i)[1];
         }
-        System.out.println();
         return answer;
     }
     
-    static void solve(int n, int a, int b) {
-        int c = 2;
-        if((a==1 && b==2)||(a==2 && b==1)) c=3;
-        if((a==1 && b==3)||(a==3 && b==1)) c=2;
-        if((a==2 && b==3)||(a==3 && b==2)) c=1;
-        
+    static void solve(int n, int s, int e, int m) {
         if(n==1) {
-            order.add(new int[]{a, b});
+            order.add(new int[]{s, e});
             return;
         }
-        if(n==2) {
-            solve(1, a, c);
-            order.add(new int[]{a, b});
-            solve(1, c, b);
-            return;
-        }
-        solve(n-1, a, c);
-        order.add(new int[]{a, b});
-        solve(n-1, c, b);
+        solve(n-1, s, m, e);
+        order.add(new int[]{s, e});
+        solve(n-1, m, e, s);
         return;
     }
 }
