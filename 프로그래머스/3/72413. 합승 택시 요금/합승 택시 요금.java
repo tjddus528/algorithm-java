6f2f249f0fc1d457bf6124012c21ad32a3a53495
Sungyeon <csy357@gmail.com>
2026-01-02 11:15:38 +0900

    [level 3] Title: 합승 택시 요금, Time: 75.03 ms, Memory: 65 MB -BaekjoonHub

---
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -0,0 +1,40 @@
+class Solution {
+    static int[][] cost;
+    public int solution(int n, int s, int a, int b, int[][] fares) {
+        cost = new int[n+1][n+1];
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n; j++) {
+                cost[i][j] = 1_000_000_000;
+            }
+        }
+        for(int[] fare: fares) {
+            cost[fare[0]][fare[1]] = fare[2];
+            cost[fare[1]][fare[0]] = fare[2];
+        }
+        for(int i=1; i<=n; i++) cost[i][i] = 0;
+        
+        for(int k=1; k<=n; k++) {
+            for(int i=1; i<=n; i++) {
+                for(int j=1; j<=n; j++) {
+                    cost[i][j] = Math.min(cost[i][j], cost[i][k]+cost[k][j]);
+                }
+            }
+        }
+        
+        // for(int i=1; i<=n; i++) {
+        //     for(int j=1; j<=n; j++) {
+        //         System.out.print(cost[i][j]+" ");
+        //     }
+        //     System.out.println();
+        // }
+        // System.out.println();
+    
+        int answer = cost[s][a] + cost[s][b];
+        for(int k=1; k<=n; k++) {
+            int c = cost[s][k] + cost[k][a] + cost[k][b];
+            if(c < 0) continue;
+            answer = Math.min(answer, cost[s][k] + cost[k][a] + cost[k][b]);
+        }
+        return answer;
+    }
+}
