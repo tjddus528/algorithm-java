4d8af880a17fc8074820e635abe9e33c6c53e917
Sungyeon <csy357@gmail.com>
2026-01-02 11:52:09 +0900

    [level 3] Title: 길 찾기 게임, Time: 37.57 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
@@ -1,113 +1,71 @@
 import java.util.*;
 class Solution {
     static class Node {
-        int value;
-        int left;
-        int right;
-        Node(int v, int l, int r) {
-            this.value = v;
-            this.left = l;
-            this.right = r;
-        }
-        @Override
-        public String toString() {
-            return "value: " + value + ", left: " + left + ", right: " + right;
+        int x, y;
+        int index;
+        Node left;
+        Node right;
+        Node(int _x, int _y, int idx) {
+            this.x = _x;
+            this.y = _y;
+            this.index = idx;
         }
     }
+    static void insertNode(Node parent, Node newNode) {
+        if(parent.x > newNode.x) {
+            if(parent.left == null) {
+                parent.left = newNode;
+                return;
+            }
+            insertNode(parent.left, newNode);
+        } else if (parent.x < newNode.x) {
+            if(parent.right == null) {
+                parent.right = newNode;
+                return;
+            }
+            insertNode(parent.right, newNode);
+        }
+    }
+    
     static List<Integer> pre = new ArrayList<>();
     static List<Integer> post = new ArrayList<>();
     public int[][] solution(int[][] nodeinfo) {
         List<int[]> nodeInfos = new ArrayList<>();
-        for(int i=0; i<nodeinfo.length; i++) {
+        for(int i=0; i<nodeinfo.length; i++)
             nodeInfos.add(new int[]{nodeinfo[i][0], nodeinfo[i][1], i+1});
-        }
-        
         
         // y좌표 내림차순, x좌표 오름차순 정렬
         Collections.sort(nodeInfos, (o1, o2) -> {
             if(o1[1]==o2[1]) return o1[0]-o2[0];
             else return o2[1]-o1[1];
         });
-        
-        // root, left, right
-        int parent = nodeInfos.get(0)[2];
-        List<int[]> left = new ArrayList<>();
-        List<int[]> right = new ArrayList<>();
-        for(int[] node: nodeInfos) {
-            if(node[0] == nodeinfo[parent-1][0]) continue;
-            else if(node[0] < nodeinfo[parent-1][0]) left.add(node);
-            else right.add(node);
+        Node root = new Node(nodeInfos.get(0)[0], nodeInfos.get(0)[1], nodeInfos.get(0)[2]);
+        for(int i=1; i<nodeInfos.size(); i++) {
+            Node node = new Node(nodeInfos.get(i)[0], nodeInfos.get(i)[1], nodeInfos.get(i)[2]);
+            insertNode(root, node);
         }
         
-        preOrder(parent, left, right, nodeinfo);
-        postOrder(parent, left, right, nodeinfo);
+        preOrder(root);
+        postOrder(root);
         
         int[][] answer = new int[2][nodeinfo.length];
         for(int i=0; i<nodeinfo.length; i++) answer[0][i] = pre.get(i);
         for(int i=0; i<nodeinfo.length; i++) answer[1][i] = post.get(i);
-        
         return answer;
         
     }
     
-    static void preOrder(int parent, List<int[]> left, List<int[]> right, int[][] nodeinfo) {
-        
-        pre.add(parent);
-        
-        if(left.size() > 0) {
-            int leftRoot = left.get(0)[2];
-            List<int[]> leftleft = new ArrayList<>();
-            List<int[]> leftright = new ArrayList<>();
-            for(int[] node: left) {
-                if(node[0] == nodeinfo[leftRoot-1][0]) continue;
-                else if(node[0] < nodeinfo[leftRoot-1][0]) leftleft.add(node);
-                else leftright.add(node);
-            }
-            preOrder(leftRoot, leftleft, leftright, nodeinfo);
-        }
-        
-        
-        if(right.size() > 0) {
-            int rightRoot = right.get(0)[2];
-            List<int[]> rightleft = new ArrayList<>();
-            List<int[]> rightright = new ArrayList<>();
-            for(int[] node: right) {
-                if(node[0] == nodeinfo[rightRoot-1][0]) continue;
-                else if(node[0] < nodeinfo[rightRoot-1][0]) rightleft.add(node);
-                else rightright.add(node);
-            }
-            preOrder(rightRoot, rightleft, rightright, nodeinfo);
-        }
+    static void preOrder(Node root) {
+        if(root == null) return;
+        pre.add(root.index);
+        preOrder(root.left);
+        preOrder(root.right);
     }
     
-    static void postOrder(int parent, List<int[]> left, List<int[]> right, int[][] nodeinfo) {
-        
-        if(left.size() > 0) {
-            int leftRoot = left.get(0)[2];
-            List<int[]> leftleft = new ArrayList<>();
-            List<int[]> leftright = new ArrayList<>();
-            for(int[] node: left) {
-                if(node[0] == nodeinfo[leftRoot-1][0]) continue;
-                else if(node[0] < nodeinfo[leftRoot-1][0]) leftleft.add(node);
-                else leftright.add(node);
-            }
-            postOrder(leftRoot, leftleft, leftright, nodeinfo);
-        }
-        
-        
-        if(right.size() > 0) {
-            int rightRoot = right.get(0)[2];
-            List<int[]> rightleft = new ArrayList<>();
-            List<int[]> rightright = new ArrayList<>();
-            for(int[] node: right) {
-                if(node[0] == nodeinfo[rightRoot-1][0]) continue;
-                else if(node[0] < nodeinfo[rightRoot-1][0]) rightleft.add(node);
-                else rightright.add(node);
-            }
-            postOrder(rightRoot, rightleft, rightright, nodeinfo);
-        }
-        
-        post.add(parent);
-        
+    static void postOrder(Node root) {
+        if(root == null) return;
+        postOrder(root.left);
+        postOrder(root.right);
+        post.add(root.index);
     }
 }
