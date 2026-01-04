ae2949d6a000061d798dace94b05ce2ed13df40e
Sungyeon <csy357@gmail.com>
2026-01-04 23:11:20 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 468 ms, Memory: 37512 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -11,52 +11,37 @@ public class Main {
     static int[][] visited;       // 해당 위치까지 도달가능한 경로의 최댓값
     static int[] dx = {0, 1, 0, -1};
     static int[] dy = {1, 0, -1, 0};
-    static class Node implements Comparable<Node>{
-        int x, y;
-        int leaf;
-        Node(int _x, int _y, int _leaf) {
-            this.x = _x;
-            this.y = _y;
-            this.leaf = _leaf;
-        }
-        @Override
-        public int compareTo(Node node) {
-            return this.leaf - node.leaf;
-        }
-    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         forest = new int[N][N];
         visited = new int[N][N];
-        int minX = 0, minY = 0;
-        int minLeaf = Integer.MAX_VALUE;
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-
-        for(int i=0; i<N; i++) {
+        for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int j=0; j<N; j++) {
+            for (int j = 0; j < N; j++) {
                 forest[i][j] = Integer.parseInt(st.nextToken());
-                pq.add(new Node(i, j, forest[i][j]));
             }
         }
-
         int answer = 0;
-        while(!pq.isEmpty()) {
-            Node cur = pq.poll();
-            for(int d=0; d<4; d++) {
-                int nx = cur.x + dx[d];
-                int ny = cur.y + dy[d];
-                if(nx<0 || ny<0 || nx>N-1 || ny>N-1) continue;
-                if(forest[nx][ny] <= forest[cur.x][cur.y]) continue;
-                if(visited[nx][ny] >= visited[cur.x][cur.y] + 1) continue;
-                visited[nx][ny] = visited[cur.x][cur.y] + 1;
-                pq.add(new Node(nx, ny, forest[nx][ny]));
-                answer = Math.max(answer, visited[nx][ny]);
-            }
-        }
-        System.out.println(answer+1);
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                answer = Math.max(answer, dfs(i, j));
+        System.out.println(answer);
 
+    }
+    static int dfs(int x, int y) {
+        if(visited[x][y] > 0) return visited[x][y];
+        visited[x][y]=1;
+
+        for(int d=0; d<4; d++) {
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+            if(nx<0 || ny<0 || nx>N-1 || ny>N-1) continue;
+            if(forest[nx][ny] <= forest[x][y]) continue;
+            visited[x][y] = Math.max(visited[x][y], dfs(nx, ny)+1);
+        }
+
+        return visited[x][y];
     }
 
 }
