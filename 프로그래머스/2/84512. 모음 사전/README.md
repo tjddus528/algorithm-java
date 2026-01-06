5da27858c79c61ffc6805ffbed9859e3458c9d8b
Sungyeon <csy357@gmail.com>
2026-01-06 22:14:06 +0900

    [level 2] Title: 모음 사전, Time: 8.33 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/README.md
+++ 프로그래머스/2/84512. 모음 사전/README.md
@@ -1,10 +1,10 @@
 # [level 2] 모음 사전 - 84512 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512) 
 
 ### 성능 요약
 
-메모리: 80.4 MB, 시간: 6.39 ms
+메모리: 88.5 MB, 시간: 8.33 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 22일 23:43:41
+2026년 01월 06일 22:14:03
 
 ### 문제 설명
 
--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,22 +1,31 @@
+import java.util.*;
 class Solution {
-    static String alphabet = "AEIOU";
-    static int order = 0;
-    static int answer = 0;
+    static char[] alpha = {'x', 'A', 'E', 'I', 'O', 'U'};
+    static int cnt;
     public int solution(String word) {
-        dup_perm("", word);
-        return answer;
+        int target = 0;
+        for(int i=0; i<word.length(); i++) {
+            for(int j=1; j<=5; j++) {
+                if(word.charAt(i) == alpha[j]) {
+                    target = target*10 + j;
+                }
+            }
+        }
+        dfs(0, target);
+        return cnt;
     }
-    static void dup_perm(String s, String word) {
-        if(s.equals(word)) {
-            answer = order;
-            return;
+    static boolean dfs(int cur, int target) {
+        if(String.valueOf(cur).length() > 5) return false;
+        cnt++;
+        for(int i=1; i<=5; i++) {
+            if((cur*10 + i) != target) {
+                if(dfs(cur*10 + i, target)) {
+                    return true;
+                }
+            }
+            else
+                return true;
         }
-        if(s.length() >= 5) return;
-        
-        for(int i=0; i<5; i++) {
-            String alpha = String.valueOf(alphabet.charAt(i));
-            order++;
-            dup_perm(s+alpha, word);
-        }
+        return false;
     }
 }
